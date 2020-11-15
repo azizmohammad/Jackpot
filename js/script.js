5823b589e4d418c97c9dbf78515f9745bacf9534
unknown <you@example.com>
2020-11-16 00:30:13 +0600

    jackpot 90% complete

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//===================================counter-up
+$('.counter').counterUp({
+    delay: 10,
+    time: 900
+});
+
+//===================================counter-down
+
+      $('.example').countdown({
+        date: '12/24/2020 23:59:59'
+        }, function () {
+          alert('Merry Christmas!');
+        });
